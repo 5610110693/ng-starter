70782d76e648f2494ff72c80b9d4297c7d4aaa44
BucBiw <thibad177@gmail.com>
2018-07-21 14:28:26 +0700

    push ngbootstrab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Ng Starter';
   x =['a', 'b'];
-  show = false;
+  show = true;
 
   debugx(event){
     let val = event.target.value;
