84fa81c33dbf30932cdb2e677365c515edf8e0ca
BucBiw <thibad177@gmail.com>
2018-07-21 14:45:57 +0700

    add ng template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { FeatureComponent } from './feature/feature.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent
+    ContactComponent,
+    NavbarComponent,
+    JumbotronComponent,
+    FeatureComponent
   ],
   imports: [
     BrowserModule,
